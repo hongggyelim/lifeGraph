c51ef935cd85045b0d322de804fbf49a7d6b5a18
hongggy <hongggy@gmail.com>
2025-02-10 13:53:58 +0900

    feat: div 위에 클릭하기

--- src/pages/Main/component/Graph.tsx
+++ src/pages/Main/component/Graph.tsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import "./graph.scss";
+import Point from "./Point";
 
 const Graph = ({ onClick }: { onClick: (x: number, y: number) => void }) => {
+  const [points, setPoints] = useState<{ x: number; y: number }[]>([]);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    setPoints((prev) => [...prev, { x, y }]); // 클릭한 좌표 저장
+  };
   return (
-    <div
-      className="graph-container"
-      onClick={(e) => {
-        const rect = e.currentTarget.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        onClick(x, y);
-      }}
-    >
+    <div className="graph-container" onClick={handleClick}>
+      {points.map((point, index) => (
+        <Point key={index} x={point.x} y={point.y} />
+      ))}
       <div className="graph-upper"></div>
       <div className="graph-below"></div>
     </div>
--- src/pages/Main/component/Point.tsx
+++ src/pages/Main/component/Point.tsx
@@ -1,5 +1,19 @@
-const Point = () => {
-  return <div>●</div>;
+const Point = ({ x, y }: { x: number; y: number }) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        width: "4px",
+        height: "4px",
+        color: "blue",
+        top: `${y - 5}px`,
+        left: `${x - 5}px`,
+        borderRadius: "50%",
+      }}
+    >
+      ●
+    </div>
+  );
 };
 
 export default Point;
