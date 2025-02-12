87e963b467ae58e468db9bbe911ce0e270824866
hongggy <hongggy@gmail.com>
2025-02-12 13:56:53 +0900

    chore: 결과 유형 지정

--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -1,37 +1,26 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { InfoType } from "../../types/userInfoType";
 import "./result.scss";
 import ResultGraph from "./Graph/ResultGraph";
 import html2canvas from "html2canvas";
 import saveAs from "file-saver";
+import useInfo from "../../hooks/useInfo";
 
 const ResultPage = () => {
-  const [info, setInfo] = useState<InfoType>({
-    name: "유저",
-    birthYear: 1990,
-  });
-  const { name } = info;
   const navigate = useNavigate();
+  const { name } = useInfo();
+  let result = String(Math.floor(Math.random() * 8));
 
-  // 페이지 로드 시 로컬 데이터 가져오기
-  useEffect(() => {
-    const storedInfo = window.localStorage.getItem("info");
-    if (storedInfo) {
-      setInfo(JSON.parse(storedInfo));
-    }
-  }, []);
-
-  const type = {
+  const resultType = {
     1: "승승장구😎",
     2: "진취적인 개혁가😎",
     3: "멋진 인생😎",
-    4: "대기만성, 화이팅!📣",
-    5: "비온뒤 맑음🌤️",
-    6: "조화로운 삶🌸",
-    7: "안정적인 성장📈",
-    8: "다채로운 삶🎨",
-  };
+    4: "조화로운 삶🌸",
+    5: "안정적인 성장📈",
+    6: "다채로운 삶🎨",
+    7: "대기만성, 화이팅!📣",
+    8: "비온뒤 맑음🌤️",
+  }[result];
 
   const imageRef = useRef<HTMLDivElement>(null);
 
@@ -67,7 +56,7 @@ const ResultPage = () => {
         <h3>{name}님의 인생 그래프</h3>
         <div className="border" ref={imageRef}>
           <b className="result-title">
-            {name}님의 인생은 {type[1]}
+            {name}님의 인생은 {resultType}
           </b>
           <ResultGraph />
           <div id="result-button-wrapper">
